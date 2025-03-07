894ee5cc3d67519724b5dac687792ad615496332
Zain <zain@cynosuredesigns.co.uk>
2025-03-07 15:59:07 +0500

    login and router updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "../styles/login.css";
+import grokIMg from "../assets/img/image.png"; // Grok logo image
+
+const LoginComponent = () => {
+  return (
+    <div className="login-container d-flex align-items-center justify-content-center">
+      <div className="login-card">
+        {/* Logo */}
+        <div className="text-center logo mb-4">
+          <img src={grokIMg} alt="Grok Logo" className="grok-logo obj_fit" />
+        </div>
+
+        {/* Social Login Buttons */}
+        <button className=" social-login">
+        <i className="fab fa-x-twitter social-icon"></i> Continue with X
+        </button>
+        <button className=" social-login">
+        <i className="fab fa-google social-icon"></i> Continue with Google
+        </button>
+        <button className=" social-login">
+        <i className="fab fa-apple social-icon"></i> Continue with Apple
+        </button>
+
+        {/* Login Form */}
+        <div className="form-group mt-4">
+          <label>Email</label>
+          <input type="email" className="form-control" placeholder="Email" />
+        </div>
+        <div className="form-group position-relative">
+          <label>Password</label>
+          <input type="password" className="form-control" placeholder="Password" />
+          <span className="material-icons password-toggle">visibility_off</span>
+        </div>
+
+        {/* Forgot Password */}
+        <div className="text-left">
+          <a href="#" className="forgot-password">Forgot your password?</a>
+        </div>
+
+        {/* Login Button */}
+        <button className="login-btn my-3">Login</button>
+
+        {/* Signup Link */}
+        <div className="text-center">
+          <p>
+            Don't have an account? <a href="#" className="signup-link">Sign up</a>
+          </p>
+        </div>
+
+        {/* Terms and Privacy */}
+        <p className="terms-text text-center">
+          By continuing, you agree to xAI's <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginComponent;
